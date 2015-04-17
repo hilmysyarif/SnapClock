551cf198d57e850288247307970747dfd334497a
vagrant <vagrant@homestead>
2015-04-17 03:28:28 +0000

    Remove public/assets for copyright issue

---
+++ resources/views/employee/edit.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sugeng
+ * Date: 3/4/2015
+ * Time: 9:53 AM
+ */
---
+++ resources/views/layouts/partials/navigation.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sugeng
+ * Date: 3/3/2015
+ * Time: 9:00 AM
+ */
